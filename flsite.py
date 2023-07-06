bb8b4abc62f23048c052eaeff9ec2373031c863b
Multilin56 <104591081+Multilin56@users.noreply.github.com>
2023-07-06 19:43:29 +0300

    Update flsite.py

--- flsite.py
+++ flsite.py
@@ -1,40 +1,46 @@
-from flask import Flask, render_template, url_for, request, flash
+import sqlite3
+import os
+from flask import Flask, render_template, request
+
+DATABASE = '/tmp/flsite.db'
+DEBUG = True
+SECRET_KEY = ''
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'mtfyjf6t7tk6f6kf6kf66r'
+app.config.from_object(__name__)
 
-menu = [{"name": "Установка", "url": "install-flask"},
-        {"name": "Первое приложение", "url": "first-app"},
-        {"name": "Обратная связь", "url": "contact"}]
+app.config.update(dict(DATABASE=os.path.join(app.root_path,'flsite.db')))
 
-@app.route('/')
+def connect_db():
+    conn = sqlite3.connect(app.config['DATABASE'])
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def create_db():
+    """Вспомогательная функция для создания таблиц БД"""
+    db = connect_db()
+    with app.open_resource('sq_db.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+    db.close()
+
+def get_db():
+    '''Соединение с БД, если оно еще не устанвлено'''
+    if not hasattr(g, 'link_db'):
+        g.link_db = connect_db()
+    return g.link_db
+
+
+@app.route("/")
 def index():
-    return render_template('index.html', menu = menu)
+    db = get_db()
+    return render_template('index.html', menu = [])
 
-@app.route("/about")
-def about():
-    return render_template('about.html', title="О сайте", menu = menu)
+@app.teardown_appcontext
+def close_db(error):
+    '''Закрываем соединение с БД, если оно было установлено'''
+    if hasattr(g, 'link_db'):
+        g.link_db.close()
 
-@app.route("/contact", methods=["POST", "GET"])
-def contact():
-    if request.method == "POST":
-        if len(request.form['username']) > 2:
-            flash('Сообщение отправлено', category='success')
-        else:
-            flash('Ошибка отправки', category='error')
-        
-    return render_template('contact.html', title="Обратная связь", menu = menu)
-
-@app.route("/profile/<username>")
-def profile(username, path):
-    return f"Пользователь: {username}, {path}"
-
-
-# with app.test_request_context():
-#     print(url_for('index'))
-#     print(url_for('about'))
-#     print(url_for('profile', username="test"))
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
