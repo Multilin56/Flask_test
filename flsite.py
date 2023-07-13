1b77f32d266cba129a2ceb3e4aed248af424dbaa
Multilin56 <104591081+Multilin56@users.noreply.github.com>
2023-07-13 20:08:10 +0300

    Update flsite.py

--- flsite.py
+++ flsite.py
@@ -5,12 +5,12 @@ from FDataBase import FDataBase
 
 DATABASE = '/tmp/flsite.db'
 DEBUG = True
-SECRET_KEY = ''
+SECRET_KEY = 'mtfyjf6t7tk6f6kf6kf66r'
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 
-app.config.update(dict(DATABASE=os.path.join(app.root_path,'flsite.db')))
+app.config.update(dict(DATABASE=os.path.join(app.root_path, 'flsite.db')))
 
 def connect_db():
     conn = sqlite3.connect(app.config['DATABASE'])
@@ -42,7 +42,34 @@ def close_db(error):
 def index():
     db = get_db()
     dbase = FDataBase(db)
-    return render_template('index.html', menu = dbase.getMenu())
+    return render_template('index.html', menu = dbase.getMenu(), posts=dbase.getPostsAnonce())
+
+@app.route("/add_post", methods=["POST","GET"])
+def addPost():
+    db = get_db()
+    dbase = FDataBase(db)
+
+    if request.method == "POST":
+        if len(request.form['name']) > 4 and len(request.form['post']) > 10:
+            res = dbase.addPost(request.form['name'], request.form['post'])
+            if not res:
+                flash('Ошибка добавления статьи', category='error')
+            else:
+                flash('Статья добавленна успешно', category='success')
+        else:
+            flash('Ошибка добавления статьи', category='error')
+
+    return render_template('add_post.html', menu = dbase.getMenu(), title="Добавление статьи")
+
+@app.route("/post/<int:id_post>")
+def showPost(id_post):
+    db = get_db()
+    dbase = FDataBase(db)
+    title, post = dbase.getPost(id_post)
+    if not title:
+        abort(404)
+
+    return render_template('post.html', menu=dbase.getMenu(), title=title, post=post)
 
 if __name__ == "__main__":
     app.run(debug=True)
