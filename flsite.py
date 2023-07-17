e1faf620e287f86008134c3c7c636e96a5f8e740
Multilin56 <104591081+Multilin56@users.noreply.github.com>
2023-07-17 17:29:14 +0300

    Update flsite.py

--- flsite.py
+++ flsite.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, make_response
+from flask import Flask, render_template, make_response, redirect, url_for
 
 app = Flask(__name__)
 
@@ -13,17 +13,46 @@ def index():
     # res.headers['Server'] = 'flasksite'
     # return res
 
-    img = None
-    with app.open_resource( app.root_path + "/static/images/ava.png", mode="rb") as f:
-        img = f.read()
+    # img = None
+    # with app.open_resource( app.root_path + "/static/images/ava.png", mode="rb") as f:
+    #     img = f.read()
+    #
+    # if img is None:
+    #     return "None image"
+    #
+    # res = make_response(img)
+    # res.headers['Content-Type'] = 'image/png'
+    # return res
 
-    if img is None:
-        return "None image"
-
-    res = make_response(img)
-    res.headers['Content_Type'] = 'image/png'
+    res = make_response("<h1>Ошибка сервера</h1>", 500)
     return res
- 
+
+@app.errorhandler(404)
+def pageNot(error):
+    return ("Страница не найдена", 404)
+
+@app.route('/transfer')
+def transfer():
+    return redirect(url_for('index'), 301)
+
+
+# @app.before_first_request
+# def before_first_request():
+#     print("before_first_request() called")
+
+@app.before_request
+def before_request():
+    print("before_request() called")
+
+@app.after_request
+def after_request(response):
+    print("after_request() called")
+    return response
+
+@app.teardown_request
+def teardown_request(response):
+    print("teardown_request() called")
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True)
