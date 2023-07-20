494884a289d1b83ea089665f84bbcd0fabc12b78
Multilin56 <104591081+Multilin56@users.noreply.github.com>
2023-07-20 21:26:30 +0300

    Update flsite.py

--- flsite.py
+++ flsite.py
@@ -1,31 +1,101 @@
-from flask import Flask, render_template, make_response, url_for, request, session
-import datetime
+import sqlite3
+import os
+from flask import Flask, render_template, request, g, flash, abort, redirect, url_for
+from FDataBase import FDataBase
+from werkzeug.security import generate_password_hash, check_password_hash
+
+DATABASE = '/tmp/flsite.db'
+DEBUG = True
+SECRET_KEY = 'mtfyjf6t7tk6f6kf6kf66r'
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '445a94fce62bb8b80c8fb47224099e163ad6080d'
-app.permanent_session_lifetime = datetime.timedelta(days=10)
+app.config.from_object(__name__)
+
+app.config.update(dict(DATABASE=os.path.join(app.root_path, 'flsite.db')))
+
+def connect_db():
+    conn = sqlite3.connect(app.config['DATABASE'])
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def create_db():
+    """Вспомогательная функция для создания таблиц БД"""
+    db = connect_db()
+    with app.open_resource('sq_db.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+    db.close()
+
+def get_db():
+    '''Соединение с БД, если оно еще не устанвлено'''
+    if not hasattr(g, 'link_db'):
+        g.link_db = connect_db()
+    return g.link_db
+
+
+dbase = None
+@app.before_request
+def before_request():
+    """Установление соединения с БД перед выплнением запроса"""
+    global dbase
+    db = get_db()
+    dbase = FDataBase(db)
+
+@app.teardown_appcontext
+def close_db(error):
+    '''Закрываем соединение с БД, если оно было установлено'''
+    if hasattr(g, 'link_db'):
+        g.link_db.close()
+
 
 @app.route("/")
 def index():
-    if 'visits' in session:
-        session['visits'] = session.get('visits') + 1  # обновление данных сессии
-    else:
-        session['visits'] = 1   # запись данных в сессию
-    return f"<h1>Main Page</h1><p>Число просмотров: {session['visits']}"
+    return render_template('index.html', menu = dbase.getMenu(), posts=dbase.getPostsAnonce())
+
+@app.route("/add_post", methods=["POST","GET"])
+def addPost():
+    if request.method == "POST":
+        if len(request.form['name']) > 4 and len(request.form['post']) > 10:
+            res = dbase.addPost(request.form['name'], request.form['post'], request.form['url'])
+            if not res:
+                flash('Ошибка добавления статьи', category='error')
+            else:
+                flash('Статья добавленна успешно', category='success')
+        else:
+            flash('Ошибка добавления статьи', category='error')
+
+    return render_template('add_post.html', menu = dbase.getMenu(), title="Добавление статьи")
+
+@app.route("/post/<alias>")
+def showPost(alias):
+    title, post = dbase.getPost(alias)
+    if not title:
+        abort(404)
+
+    return render_template('post.html', menu=dbase.getMenu(), title=title, post=post)
+
+@app.route("/login")
+def login():
+    return render_template('login.html', menu=dbase.getMenu(), title="Авторизация")
+
+@app.route("/register", methods=['POST', 'GET'])
+def register():
+    if request.method == "POST":
+        if len(request.form['name']) > 4 and len(request.form['email']) > 4 \
+            and len(request.form['psw']) > 4 and request.form['psw'] == request.form['psw2']:
+            hash = generate_password_hash(request.form['psw'])
+            res = dbase.addUser(request.form['name'], request.form['email'], hash)
+            if res:
+                flash("Вы успешно зарегестрированы", "success")
+                return redirect(url_for('login'))
+            else:
+                print(res)
+                flash("Ошибка при добавлении в БД", "error")
+        else:
+            flash("Неверно заполнены поля", "error")
+
+    return render_template('register.html', menu=dbase.getMenu(), title="Регистрация")
 
 
-data = [1,2,3,4]
-@app.route("/session")
-def session_data():
-    session.permanent = True
-    if 'data' not in session:
-        session['data'] = data
-    else:
-        session['data'][1] += 1
-        session.modified = True
-
-    return f"<p>session['data']:  {session['data']}"
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
